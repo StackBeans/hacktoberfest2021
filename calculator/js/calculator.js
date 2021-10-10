68a9bb8b7a0f61acd9aa229362c63b1fb6379eb0
Shariq Ahmed <shariq.ahmed525@gmail.com>
2021-10-10 21:07:36 +0500

    fixed double operand acceptance issue & added decimal operator

--- calculator/js/calculator.js
+++ calculator/js/calculator.js
@@ -1,98 +1,109 @@
-//
 function getNumber(num) {
-    var manal = document.getElementById('input');
-    switch (num) {
-        case 1:
-            manal.value += '1';
-            break;
-        case 2:
-            manal.value += '2';
-            break;
-        case 3:
-            manal.value += '3';
-            break;
-        case 4:
-            manal.value += '4';
-            break;
-        case 5:
-            manal.value += '5';
-            break;
-        case 6:
-            manal.value += '6';
-            break;
-        case 7:
-            manal.value += '7';
-            break;
-        case 8:
-            manal.value += '8';
-            break;
-        case 9:
-            manal.value += '9';
-            break;
-        case 0:
-            manal.value += '0';
-            break;
-    }
+  var input = document.getElementById("input");
+  var getLastChar = input.value.toString().slice(-1);
+
+  switch (num) {
+    case 1:
+      input.value += "1";
+      break;
+    case 2:
+      input.value += "2";
+      break;
+    case 3:
+      input.value += "3";
+      break;
+    case 4:
+      input.value += "4";
+      break;
+    case 5:
+      input.value += "5";
+      break;
+    case 6:
+      input.value += "6";
+      break;
+    case 7:
+      input.value += "7";
+      break;
+    case 8:
+      input.value += "8";
+      break;
+    case 9:
+      input.value += "9";
+      break;
+    case 0:
+      input.value += "0";
+      break;
+    case ".":
+      if (getLastChar === "." || input.value.toString().includes(".")) {
+        return;
+      }
+      input.value += ".";
+      break;
+  }
 }
 
 /* clear screen CE button function */
 function clearScreen() {
-    document.getElementById('input').value = "";
-    document.getElementById('answer').value = "";
+  document.getElementById("input").value = "";
+  document.getElementById("answer").value = "";
 }
 
 /* for getting the mathematical sign operator */
 function getOperand(operand) {
-    var manal = document.getElementById('input');
-    switch (operand) {
-        case '+':
-            manal.value += '+';
-            break;
-        case '-':
-            manal.value += '-';
-            break;
-        case '*':
-            manal.value += '*';
-            break;
-        case '/':
-            manal.value += '/';
-            break;
-        case '+/-':
-            manal.value += '-' + manal.value;
-            break;
-    }
+  var input = document.getElementById("input");
+  var getLastChar = input.value.toString().slice(-1);
+  if (
+    getLastChar.includes("+") ||
+    getLastChar.includes("-") ||
+    getLastChar.includes("*") ||
+    getLastChar.includes("/")
+  ) {
+    return;
+  }
+  switch (operand) {
+    case "+":
+      input.value += "+";
+      break;
+    case "-":
+      input.value += "-";
+      break;
+    case "*":
+      input.value += "*";
+      break;
+    case "/":
+      input.value += "/";
+      break;
+  }
 }
 
 /* delete a digit function */
 function backspace() {
-    var manal = document.getElementById('input');
-    var x = manal.value;
-    if (x.length > 0) {
-        x = x.substring(0, x.length-1); /* removes the last digit */
-        manal.value = x;
-    }
+  var input = document.getElementById("input");
+  var x = input.value;
+  if (x.length > 0) {
+    x = x.substring(0, x.length - 1); /* removes the last digit */
+    input.value = x;
+  }
 }
 
 function compute() {
-    var manal = document.getElementById('input');
-    ans =  +eval(manal.value).toFixed(11);
-    document.getElementById('answer').value = '= ' + ans;
+  var input = document.getElementById("input");
+  ans = +eval(input.value).toFixed(11);
+  document.getElementById("answer").value = "= " + ans;
 }
 
 var i = 0;
 function brackets() {
-    var manal = document.getElementById('input');
-    if (i == 0) {
-        manal.value += '(';
-        i = 1;
-    }else if (i == 1) {
-        manal.value += ')';
-        i = 0;
-    }
+  var input = document.getElementById("input");
+  if (i == 0) {
+    input.value += "(";
+    i = 1;
+  } else if (i == 1) {
+    input.value += ")";
+    i = 0;
+  }
 }
 
 document.oncontextmenu = function () {
-    return false;
-}
-
-
+  return false;
+};
